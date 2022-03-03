a2f1bb4953157140a38822da0c4f57b71bfb87f5
varakarapureddy <99267924+varakarapureddy@users.noreply.github.com>
2022-03-03 20:43:10 +0530

    done assigment 2

---
+++ assigment2.py
@@ -0,0 +1,9 @@
+veg=("carrot","beetroot","tomato")
+beet=("chicken","mutton","fish")
+print("pure vegeritans eats only")
+print(veg)
+print("pure nonvegritans eats only")
+print(beet)
+nonveg=veg+beet
+print("human eats")
+print(nonveg)
